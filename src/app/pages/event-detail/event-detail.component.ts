a88f557dba6208021a8c7a19e3629968397c784e
hugo-bourgey <92681630+hugo-bourgey@users.noreply.github.com>
2024-01-09 17:14:22 +0100

    Event service (WIP)

--- src/app/pages/event-detail/event-detail.component.ts
+++ src/app/pages/event-detail/event-detail.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { OnInit } from '@angular/core';
 import { Event } from '../../models/event.model';
+import { EventService } from '../../services/event.service';
 
 @Component({
   selector: 'app-event-detail',
@@ -12,17 +13,17 @@ import { Event } from '../../models/event.model';
   styleUrl: './event-detail.component.css'
 })
 export class EventDetailComponent {
-  eventId = null;
   
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private eventService: EventService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       // Access the 'id' parameter from the route
-      this.eventId = params['id'];
-  
-      // Now you can use the eventId as needed in your component
-      console.log('Event ID:', this.eventId);
+      const eventId = params['id'];
+
+      let event = this.eventService.getEventById(eventId);
+
+      console.log(event);
 
     });
   }
