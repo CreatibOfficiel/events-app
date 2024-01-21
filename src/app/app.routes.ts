bea2cf751573c9691169c59541b9af42908349fa
Thibaud <creatibofficiel@gmail.com>
2024-01-21 18:44:24 +0100

    feat(register steps) : select tags & user type

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,10 @@ import { FeedComponent } from './pages/feed/feed.component';
 import { CalendarComponent } from './pages/calendar/calendar.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { SearchComponent } from './pages/search/search.component';
-import {LoginComponent} from "./pages/auth/login/login.component";
-import {RegisterComponent} from "./pages/auth/register/register.component";
+import { LoginComponent } from "./pages/auth/login/login.component";
+import { RegisterComponent } from "./pages/auth/register/register.component";
+import { SelectTagsComponent } from './pages/select-tags/select-tags.component';
+import { SelectUserTypeComponent } from './pages/select-user-type/select-user-type.component';
 
 export const routes: Routes = [
     { path: '', component: FeedComponent },
@@ -12,5 +14,7 @@ export const routes: Routes = [
     { path: 'profile', component: ProfileComponent },
     { path: 'search', component: SearchComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent }
+    { path: 'register', component: RegisterComponent },
+    { path: 'select-tags', component: SelectTagsComponent },
+    { path: 'select-user-type', component: SelectUserTypeComponent },
 ];
